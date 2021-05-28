b9489d7c3936b93aa344500f8421933babb6d6da
Dániel Mihalik <dani.mihalik@gmail.com>
2021-05-28 18:58:53 +0100

    implement lib interaction

--- Makefile
+++ Makefile
@@ -1,10 +1,14 @@
 CC = gcc
-CFLAGS = -Wall -g
-BUILD = main
+CFLAGS = -Wall -g -Ilib
+LDLIBS = -Llib -lutils
+BUILD = libs main
 
 all: $(BUILD)
-clear:
+clean:
 	rm -f $(BUILD) *.o
+	cd lib; make clean
+libs:
+	cd lib; make
 
 main: main.o
 main.o: main.c
--- main.c
+++ main.c
@@ -1,7 +1,15 @@
 #include <stdio.h>
 
+#include "add.h"
+#include "mult.h"
+
 int main(void){
-	printf("Hello, this is still not working!\n");
+	int testA = add(1, 2);
+	int testB = mult(2, 3);
+	
+	printf("1 + 2 = %d\n", testA);
+	printf("2 * 3 = %d\n", testB);
+
 	return 0;
 }
 
